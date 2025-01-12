a056cc5538c38fa2704eb230747dc286932bc4a2
theanywayguy <ajajiphone.am@gmail.com>
2025-01-12 13:14:33 +0300

    new applications

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,17 @@
 
 from django.shortcuts import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return HttpResponse("hello,world")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("hello,brian")
+
+def david(request):
+    return HttpResponse("Hello,David")
+
+def greet(request,name):
+    return render(request,"hello/greet.html",{
+       "name": name.capitalize() 
+    })
